709401743dae058275b3ba53034bad9d57e6e1bd
annaannasoftware <76137073+annaannasoftware@users.noreply.github.com>
2021-04-28 20:00:29 -0400

    cccftrdaqzswedrcftyvgbhunj

---
+++ js/html_actuator.js
@@ -0,0 +1,38 @@
+function HTMLActuator () {
+    this.tileContainer = document.querySelector (".tile-container");
+    this.scoreContainer = document.querySelector (".score-container");
+    this.bestContainer = document.querySelector (".best-container");
+    this.messageContainer = document.querySelector (".game-message");
+    this.sharingContainer = document.querySelector(".score-sharing");
+
+    this.score = 0;
+}
+
+HTMLActuator.prototype.actuate = function (grid, metadata) {
+    var self = this;
+
+    window.requestAnimationFrame (function () {
+        self.clearContainer (self.tileContainer);
+
+        grid.cells.forEach (function (column) {
+            column.forEach (function (cell) {
+                if (cell) {
+                    self.addTile (cell);
+                }
+            });
+        });
+
+        self.updateScore (metadata.score);
+        self.updateBestScore (metadata.bestScore);
+        
+        if (metadata.terminated) {
+            if (metadata.over) {
+                self.message (false);
+            } else if (metadata.won) {
+                self.message (true);
+            }
+        }
+
+    });
+};
+
